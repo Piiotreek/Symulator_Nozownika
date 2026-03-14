3a09beaa954c79c9c694dd1173d024a99edf91e6
Piiotreek <poletylopiotr@gmail.com>
2026-03-14 15:11:14 +0100

    Dodaj pliki projektów.

---
+++ Symulator_Nozownika/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Symulator_Nozownika.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
