f462034dd952de891d5e6acd2207ac5af4cf5087
ErythrinaCremesinus <strzelecpp@gmail.com>
2026-04-01 11:17:30 +0200

    Lepszy pasek główny

--- Symulator_Nozownika/Migrations/20260331145512_NowaMigracjaUsunPotem.Designer.cs
+++ Symulator_Nozownika/Migrations/20260331145512_NowaMigracjaUsunPotem.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Symulator_Nozownika.Data;
 
@@ -10,9 +11,11 @@ using Symulator_Nozownika.Data;
 namespace Symulator_Nozownika.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260331145512_NowaMigracjaUsunPotem")]
+    partial class NowaMigracjaUsunPotem
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Symulator_Nozownika/Migrations/20260331145512_NowaMigracjaUsunPotem.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Symulator_Nozownika.Migrations
+{
+    /// <inheritdoc />
+    public partial class NowaMigracjaUsunPotem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_UserAccounts_SelectedWeaponId",
+                table: "UserAccounts",
+                column: "SelectedWeaponId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserAccounts_Weapons_SelectedWeaponId",
+                table: "UserAccounts",
+                column: "SelectedWeaponId",
+                principalTable: "Weapons",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserAccounts_Weapons_SelectedWeaponId",
+                table: "UserAccounts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_UserAccounts_SelectedWeaponId",
+                table: "UserAccounts");
+        }
+    }
+}
