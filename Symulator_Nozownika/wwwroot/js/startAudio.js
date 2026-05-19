d5e83de740fca2bcf2da1697d540ac188fd51f59
ErythrinaCremesinus <strzelecpp@gmail.com>
2026-05-19 12:54:59 +0200

    Nowe dźwięki

--- Symulator_Nozownika/wwwroot/js/startAudio.js
+++ Symulator_Nozownika/wwwroot/js/startAudio.js
@@ -2,4 +2,10 @@
     const loginMusic = new Audio('/audio/startviewaudio.mp3');
     loginMusic.loop = true;
     loginMusic.volume = 0.5;
+
+    const buttonhover = new Audio('/audio/buttonhover.mp3');
+    buttonhover.volume = 1;
+
+    const buttonclick = new Audio('/audio/buttonclick.mp3');
+    buttonclick.volume = 1;
 });
